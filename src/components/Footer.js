e7bf4861f13e38cab6b226fe3e11f9b5a697184a
sw-yx <swyx@swyx.io>
2017-10-23 14:25:28 -0400

    add footer stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,41 +6,50 @@ const Footer = () => (
     <div className="inner">
       <ul className="icons">
         <li>
-          <a href="#" className="icon alt fa-envelope">
+          <a href="mailto:swyx-at-swyx-dot-io" className="icon alt fa-envelope">
             <span className="label">Email</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon alt fa-twitter">
+          <a
+            href="http://twitter.com/swyx"
+            target="_blank"
+            className="icon alt fa-twitter"
+          >
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon alt fa-facebook">
-            <span className="label">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" className="icon alt fa-instagram">
-            <span className="label">Instagram</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" className="icon alt fa-github">
+          <a
+            href="https://github.com/sw-yx/"
+            target="_blank"
+            className="icon alt fa-github"
+          >
             <span className="label">GitHub</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon alt fa-linkedin">
+          <a
+            href="https://www.linkedin.com/in/shawnswyxwang/"
+            target="_blank"
+            className="icon alt fa-linkedin"
+          >
             <span className="label">LinkedIn</span>
           </a>
         </li>
       </ul>
       <ul className="copyright">
         <li>&copy; swyx</li>
-        <li>Best contacted on Twitter</li>
         <li>
-          Design: <a href="https://html5up.net">HTML5 UP</a>
+          <a href="http://twitter.com/swyx" target="_blank">
+            Best contacted on Twitter
+          </a>
+        </li>
+        <li>
+          Design:{" "}
+          <a href="https://html5up.net" target="_blank">
+            HTML5 UP
+          </a>
         </li>
       </ul>
     </div>
