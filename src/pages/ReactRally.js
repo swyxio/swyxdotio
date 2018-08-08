244eb4813ad1700ef5db4d0c2a1aa68e06647cda
sw-yx <swyx@swyx.io>
2018-08-08 17:08:54 -0400

    reactrally

---
+++ src/pages/ReactRally.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import { A } from 'swyx-a'
+
+// import { ReactTypeformEmbed } from 'react-typeform-embed';
+// import * as typeformEmbed from '@typeform/embed'
+
+function ReactRally() {
+  return (
+  <div
+    style={{
+      position: 'relative',
+      width: '100%',
+      height: 0,
+      paddingBottom: 'calc(56.25% + 40px)',
+    }}
+  >
+  <h1>Why React is -not- Reactive</h1>
+  <h5>a talk given at ReactNYC/React Rally</h5>
+    <p>
+    diving into React's push-pull data flow and the importance of the scheduler: 
+           <A href="https://github.com/sw-yx/reactive-react">
+             reactive-react
+           </A>
+    </p>
+    <p style={{paddingLeft: 100}}>
+    Feedback: <button 
+      style={{
+        backgroundColor: 'lightblue',
+        borderRadius: 10,
+        padding: 20
+      }}
+      onClick={() => window.open("https://shawnwang3.typeform.com/to/cC7sYD")}
+      ><marquee>
+        <h3>⚛️ GIVE FEEDBACK! ⚛️</h3>
+        </marquee>
+    </button>
+    </p> 
+    <iframe src="//slides.com/swyx/why-react-is-not-reactive/embed" width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
+)
+}
+export default ReactRally
