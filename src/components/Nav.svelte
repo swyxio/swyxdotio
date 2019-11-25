f89858d41c9815985d981defca62ec9427702715
sw-yx <swyx@swyx.io>
2019-11-26 00:24:38 +0800

    i'm an idiot

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -226,7 +226,11 @@
       </a>
     </li>
     <li class="external block">
-      <a href="https://twitter.com/swyx" title="twitter" target="_blank" rel="noopener">
+      <a
+        href="https://twitter.com/swyx"
+        title="twitter"
+        target="_blank"
+        rel="noopener">
         <svg
           class="icon"
           viewBox="0 0 24 24"
@@ -245,7 +249,7 @@
     </li>
     <li class="external block">
       <a
-      title="github source of this site"
+        title="github source of this site"
         href="https://github.com/sw-yx/swyxdotio"
         target="_blank"
         rel="noopener">
@@ -268,15 +272,21 @@
       </a>
     </li>
     <li class="external block">
-      <a
-      title="search"
-        href="/search"><svg 
+      <a title="search" href="/search">
+        <svg
           class="icon"
           viewBox="-2 -2 24 24"
           stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
-          aria-hidden="true"><path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"/></svg>
+          aria-hidden="true">
+          <path
+            d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243
+            0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923
+            4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0
+            8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5
+            8.5-8.5z" />
+        </svg>
       </a>
     </li>
   </ul>
