31dca3f06b097bc0df6e66b69b53c660f17a1c5f
swyx <shawnthe1@gmail.com>
2020-02-11 21:37:54 -0500

    add clientside webmentions

---
+++ content/writing/technical/webmentions-clientside.md
@@ -0,0 +1,212 @@
+---
+title: Clientside Webmentions
+slug: clientside-webmentions
+categories: ['IndieWeb']
+date: 2020-02-11
+description: How you can enhance your blog with webmentions without adding heavy build times.
+---
+
+[Max Bock's blogpost on Webmentions](https://mxb.dev/blog/using-webmentions-on-static-sites/) was very influential in the Indieweb community, but it discusses a serverside only approach to pulling and displaying that data.
+
+I was interested in deferring that work and leaving it to the clientside. Clientside solutions of course forces clients to do more work, but explicitly designing for it can be more considerate by not dumping a ton of image requests and useless markup on the mobile user. Of course, build times and success rates are also a little more predictable if you take external dependencies out of the equation.
+
+I searched Webmention implementations on GitHub and found [Max Stoiber's impl](https://github.com/mxstbr/mxstbr.com/blob/49aceb93a43d1e87736f204f26c07e203cb2a0e1/components/WebMentions/WebMentionCounts.js). It fetches a simple count of webmentions, and then paginated full text responses. I figured I should try adapting that.
+
+> ⚠️ For this post I will assume you've already followed Max's advice on setting up [webmention.io](https://mxb.dev/blog/using-webmentions-on-static-sites/) (add the twitter link with `rel="me"`) and then setting up the backfeed with [Bridgy](https://brid.gy/). I don't know any other services that perform these functions - we have to be thankful to folks like [Aaron Parecki](https://github.com/aaronpk) for the fact that these even work.
+
+## Simple Count
+
+This is the endpoint to hit: `https://webmention.io/api/count.json?target=URL_TO_YOUR_POST/`. ⚠️ NOTE: You will need that trailing slash for this request to work! I probably wasted 2 hours figuring this out.
+
+This is the API response you get back:
+
+```json
+{
+  "count": 1062,
+  "type": {
+    "like": 638,
+    "mention": 154,
+    "reply": 51,
+    "repost": 219
+  }
+}
+```
+
+Some of these are likes, and retweets, while others are replies and quote tweets. Max combines the last 2 and first 2 and I do so too.
+
+```html
+<script>
+  export let target // passed in as prop
+  const counts = fetch(`https://webmention.io/api/count.json?target=${target}`)
+    .then(res => res.json())
+    .then(x => x.type)
+</script>
+{#await counts}
+  <p>loading counts</p>
+{:then data}
+  {#if data === undefined}
+    Failed to load...
+  {:else}
+    ❤️ {data.like + data.repost || 0} 💬 {data.mention + data.reply || 0}
+  {/if}
+{/await}
+```
+
+## Paginated Mentions
+
+Of course, counts are nice, but real human contact lives in mentions. Here is the endpoint to hit: `https://webmention.io/api/mentions?page=0&per-page=20&sort-by=published&target=URL_TO_YOUR_POST/`
+
+> ⚠️ NOTE: You will need that trailing slash for this request to work! I probably wasted 2 hours figuring this out.
+
+> ⚠️ Note that the endpoint is `/mentions` - the docs say to hit `/mentions.jf2` but that did not work at all in my testing. 
+
+Note that there is pagination, so you have to be able to increment the page and refetch the mentions. 
+
+I originally chose to use Svelte's nifty [#await syntax](https://svelte.dev/docs#await) - this means only showing 20 at any time:
+
+```html
+<script>
+  let page = 0
+  export let target
+  const promiseFactory = () =>
+    fetch(`https://webmention.io/api/mentions?page=${page}&target=${target}`)
+      .then(x => x.json())
+      .then(x => x.links) // array
+  let promise = promiseFactory()
+  const dec = () => {
+    page -= 1
+    promise = promiseFactory()
+  }
+  const inc = () => {
+    page += 1
+    promise = promiseFactory()
+  }
+</script>
+{#await promise}
+  <p>Loading replies...</p>
+{:then links}
+  {#if links.length === 0}
+    <div>
+      No replies yet.
+      <a
+        href="https://twitter.com/intent/tweet/?text=My%20thoughts%20on%20{target}">
+        Tweet about this post
+      </a>
+      and it will show up here!
+    </div>
+  {:else}
+    <div>
+      <button on:click={dec} disabled={page === 0}>-</button>
+      Page {page + 1}
+      <button on:click={inc}>+</button>
+    </div>
+    <h1>Replies</h1>
+    <ul>
+      {#each links as link}
+        <li>
+          <div width="40">
+            <a
+              target="_blank"
+              rel="noopener"
+              href={link.data.url}
+              color="blue">
+              <img
+                width="40"
+                height="40"
+                alt="avatar of {link.data.author.name}"
+                src={link.data.author.photo} />
+            </a>
+          </div>
+          <div>
+            <a
+              target="_blank"
+              rel="noopener"
+              href={link.data.url}
+              color="blue">
+              <div font-family="system" color="text" font-weight="bold">
+                {link.data.author.name}
+                <span color="tertiary">
+                  · {new Date(link.data.published)}
+                </span>
+              </div>
+            </a>
+            <div>
+              <p font-family="system" color="tertiary" font-size="2">
+                {@html link.activity.sentence_html}
+              </p>
+            </div>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+{:catch error}
+  <!-- promise was rejected -->
+  <p>Something went wrong: {error.message}</p>
+{/await}
+```
+
+But this means losing the previous 20 that you see and also some nasty re-layout issues.
+
+Because I am SSRing, I also had some nasty `fetch is undefined` error issues.
+
+Instead I switched to another implementation that appended infinitely to an array, and loaded `onMount`:
+
+```html
+<script>
+  let page = 0
+  export let target
+  let counts
+  let mentions = []
+  let fetchState = 'fetching'
+  import { onMount } from 'svelte'
+  onMount(() => {
+    counts = fetch(`https://webmention.io/api/count.json?target=${target}/`) // trailing slash impt
+      .then(res => res.json())
+      .then(x => x.type)
+    getMentions().then(x => {
+      mentions = x
+      fetchState = 'done'
+    })
+  })
+  function getMentions() {
+    return fetch(
+      // `https://webmention.io/api/mentions?page=${page}&per-page=20&sort-by=published&target=${target}`,
+      `https://webmention.io/api/mentions?page=${page}&per-page=50&target=${target}/` // trailing slash impt
+    )
+      .then(x => x.json())
+      .then(x => x.links.filter(x => x.activity.type !== 'like'))
+  }
+  const fetchMore = () => {
+    page += 1
+    getMentions().then(x => {
+      if (x.length) {
+        mentions = [...mentions, ...x]
+      } else {
+        fetchState = 'nomore'
+      }
+    })
+  }
+</script>
+<!-- etc -->
+
+    {#if fetchState !== 'nomore'}
+      <li>
+        <button class="FetchMore" on:click={fetchMore}>
+          Fetch More...
+        </button>
+      </li>
+    {:else}
+      <li>
+        No further replies found.
+        <a
+          href="https://twitter.com/intent/tweet/?text=My%20thoughts%20on%20{target}">
+          Tweet about this post
+        </a>
+        and it will show up here!
+      </li>
+    {/if}
+```
+
+
+And that's the clientside webmentions you see live now 👇🏽.
--- src/components/WebMentions.svelte
+++ src/components/WebMentions.svelte
@@ -2,24 +2,41 @@
   let page = 0
   export let target
   if (!target) console.error('error: no target')
-  const counts = fetch(`https://webmention.io/api/count.json?target=${target}`)
-    .then(res => res.json())
-    .then(x => x.type)
-  const promiseFactory = () =>
-    fetch(
+  let counts
+  let mentions = []
+  let fetchState = 'fetching'
+  import { onMount } from 'svelte'
+  onMount(() => {
+    counts = fetch(`https://webmention.io/api/count.json?target=${target}/`) // trailing slash impt
+      .then(res => res.json())
+      .then(x => x.type)
+    getMentions().then(x => {
+      mentions = x
+      fetchState = 'done'
+    })
+  })
+  function getMentions() {
+    return fetch(
       // `https://webmention.io/api/mentions?page=${page}&per-page=20&sort-by=published&target=${target}`,
-      `https://webmention.io/api/mentions?page=${page}&target=${target}`
+      `https://webmention.io/api/mentions?page=${page}&per-page=50&target=${target}/` // trailing slash impt
     )
       .then(x => x.json())
-      .then(x => x.links) // array
-  let promise = promiseFactory()
-  const dec = () => {
-    page -= 1
-    promise = promiseFactory()
+      .then(x => x.links.filter(x => x.activity.type !== 'like'))
   }
-  const inc = () => {
+  const fetchMore = () => {
     page += 1
-    promise = promiseFactory()
+    getMentions().then(x => {
+      if (x.length) {
+        mentions = [...mentions, ...x]
+      } else {
+        fetchState = 'nomore'
+      }
+    })
+  }
+  function cleanString(str) {
+    const withSlash = target + '/'
+    const linky = `<a href="${withSlash}">${withSlash}</a>`
+    return str.replace(linky, '')
   }
 </script>
 
@@ -37,55 +54,89 @@
     border: 1px dashed var(--link-color);
     padding: 1rem;
     margin-top: 1rem;
+    color: var(--text-color);
+  }
+  .WebMentionsContainer ul {
+    list-style-type: none;
+    padding: 0;
   }
   hr {
     margin: 3rem 1rem;
+  }
+  .WebMentionReply {
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .Avatar {
+    margin-right: 16px;
+    width: 40px;
+    flex: 0 0 auto;
+    align-self: start;
+  }
+  .Avatar img {
+    width: 40px;
+    max-width: 100%;
+    height: 40px;
+    margin: 0px;
+    border-radius: 50%;
+  }
+  .WebMentionsHeader {
+    display: flex;
+    justify-content: space-between;
+  }
+  .FetchMore {
+    width: 20ch;
+    font-size: 1.5rem;
+    background-color: transparent;
+    color: var(--link-color);
+    border: 0;
+    text-align: left;
   }
 </style>
 
 <hr />
 <div id="WebMentions">
-  <h3
-    font-family="system"
-    font-size="4"
-    font-weight="bold"
-  >
-    Webmentions 
-  </h3>
+  <h3 font-family="system" font-size="4" font-weight="bold">Webmentions</h3>
   <a
-      target="_blank"
-      rel="noopener"
-      href="https://indieweb.org/Webmention"
-      color="blue">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="0.75em"
-          height="0.75em"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#999"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
-          <circle cx="12" cy="12" r="10" />
-          <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-          <line x1="12" y1="17" x2="12" y2="17" />
-        </svg>
-    </a>
+    target="_blank"
+    rel="noopener"
+    href="https://indieweb.org/Webmention"
+    color="blue">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="0.75em"
+      height="0.75em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#999"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <line x1="12" y1="17" x2="12" y2="17" />
+    </svg>
+  </a>
   <div class="WebMentionsContainer">
-  {#await counts}
-    <p>loading counts</p>
-  {:then data}
-    {#if data === undefined}
-      Failed to load...
-    {:else}
-      ❤️ {data.like + data.repost || 0} 💬 {data.mention + data.reply || 0}
-    {/if}
-  {/await}
-  {#await promise}
-    <p>Loading replies...</p>
-  {:then links}
-    {#if links.length === 0}
+    <div class="WebMentionsHeader">
+      {#await counts}
+        <p>loading counts</p>
+      {:then data}
+        <div>
+          {#if data === undefined}
+            Failed to load...
+          {:else}
+            ❤️ {data.like + data.repost || 0} 💬 {data.mention + data.reply || 0}
+          {/if}
+        </div>
+      {/await}
+    </div>
+    {#if fetchState === 'fetching'}
+
+    {:else if mentions.length === 0}
       <div>
         No replies yet.
         <a
@@ -95,16 +146,10 @@
         and it will show up here!
       </div>
     {:else}
-      <div>
-        <button on:click={dec} disabled={page === 0}>-</button>
-        Page {page + 1}
-        <button on:click={inc}>+</button>
-      </div>
-      <h1>Replies</h1>
       <ul>
-        {#each links as link}
-          <li>
-            <div width="40">
+        {#each mentions as link}
+          <li class="WebMentionReply">
+            <div class="Avatar">
               <a
                 target="_blank"
                 rel="noopener"
@@ -118,31 +163,55 @@
               </a>
             </div>
             <div>
-              <a
-                target="_blank"
-                rel="noopener"
-                href={link.data.url}
-                color="blue">
+              {#if link.activity.type === 'repost'}
+                {link.data.author.name}
+                <a href={link.data.url}>retweeted</a>
+                on {link.data.published.slice(0, 10)}
+              {:else}
                 <div font-family="system" color="text" font-weight="bold">
                   {link.data.author.name}
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href={link.data.url}
+                    color="blue">
+                    replied
+                  </a>
+                  on
                   <span color="tertiary">
-                    · {new Date(link.data.published)}
+                    · {link.data.published.slice(0, 10)}
                   </span>
                 </div>
-              </a>
-              <div>
-                <p font-family="system" color="tertiary" font-size="2">
-                  {@html link.activity.sentence_html}
-                </p>
-              </div>
+
+                <div>
+                  <blockquote
+                    font-family="system"
+                    color="tertiary"
+                    font-size="2">
+                    {@html cleanString(link.data.content)}
+                  </blockquote>
+                </div>
+              {/if}
             </div>
           </li>
         {/each}
+        {#if fetchState !== 'nomore'}
+          <li>
+            <button class="FetchMore" on:click={fetchMore}>
+              Fetch More...
+            </button>
+          </li>
+        {:else}
+          <li>
+            No further replies found.
+            <a
+              href="https://twitter.com/intent/tweet/?text=My%20thoughts%20on%20{target}">
+              Tweet about this post
+            </a>
+            and it will show up here!
+          </li>
+        {/if}
       </ul>
     {/if}
-  {:catch error}
-    <!-- promise was rejected -->
-    <p>Something went wrong: {error.message}</p>
-  {/await}
-    </div>
+  </div>
 </div>
