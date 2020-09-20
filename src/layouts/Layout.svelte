cfb20f7f9ef6da68744aa67b65298241c64f6fac
sw-yx <shawnthe1@gmail.com>
2020-09-21 04:00:52 +0800

    add scrollbars

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -11,6 +11,75 @@
   /* purgecss end ignore */
 
   @tailwind utilities;
+
+
+      
+  /* https://css-tricks.com/the-current-state-of-styling-scrollbars/ */
+  body::-webkit-scrollbar-track {
+    background-color: var(--bg-color);
+  }
+  body::-webkit-scrollbar {
+    width: 2vw;
+  }
+  body::-webkit-scrollbar-thumb {
+    background: linear-gradient(var(--link-color), var(--text-color));
+  }
+@media (min-width: 1000px) {
+  body::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.9);
+    background:
+      radial-gradient(circle closest-side at 60% 43%, var(--bg-color) 26%, rgba(187,0,51,0) 27%),
+      radial-gradient(circle closest-side at 40% 43%, var(--bg-color) 26%, rgba(187,0,51,0) 27%),
+      radial-gradient(circle closest-side at 40% 22%, #d35 45%, rgba(221,51,85,0) 46%),
+      radial-gradient(circle closest-side at 60% 22%, #d35 45%, rgba(221,51,85,0) 46%),
+      radial-gradient(circle closest-side at 50% 35%, #d35 30%, rgba(221,51,85,0) 31%),
+
+      radial-gradient(circle closest-side at 60% 43%, var(--bg-color) 26%, rgba(187,0,51,0) 27%) 50px 50px,
+      radial-gradient(circle closest-side at 40% 43%, var(--bg-color) 26%, rgba(187,0,51,0) 27%) 50px 50px,
+      radial-gradient(circle closest-side at 40% 22%, #d35 45%, rgba(221,51,85,0) 46%) 50px 50px,
+      radial-gradient(circle closest-side at 60% 22%, #d35 45%, rgba(221,51,85,0) 46%) 50px 50px,
+      radial-gradient(circle closest-side at 50% 35%, #d35 30%, rgba(221,51,85,0) 31%) 50px 50px;
+    background-color: var(--bg-color);
+    background-size: 20px 20px;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 10px rgba(0,0,0,0.9);
+    border-radius: 10px;
+    background-image: -webkit-linear-gradient(45deg,
+                              rgba(170, 170, 225, .7) 25%,
+                              rgba(100, 50, 50, .9) 25%,
+                              rgba(100, 50, 50, .9) 50%,
+                              rgba(170, 170, 225, .7) 50%,
+                              rgba(170, 170, 225, .7) 75%,
+                              rgba(100, 50, 50, .9) 75%,
+                              rgba(100, 50, 50, .9));
+    background-size: 2rem 2rem;
+  }
+}
+
+
+/* page border */
+body:before {
+  content: '';
+  position: fixed;
+  max-width: 100vw;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  border-radius: inherit;
+  /* background: linear-gradient(var(--brand-color-primary), #9198e5); */
+  background: linear-gradient(#ff335f, #9198e5);
+}
+
+#base {
+  transition: background-color 1s ease;
+  background: var(--bg-color);
+  min-height: calc(100vh - 10px);
+  margin: 5px;
+}
 </style>
 
 <svelte:head>
@@ -24,7 +93,7 @@
     }
   </script>
 </svelte:head>
-<div>
+<div id="base">
   <div class="relative bg-gray-800 overflow-hidden">
     <div class="relative pt-6 pb-12 sm:pb-32">
       <MobileMenu hydrate-client={{}} />
