029f426da6f8f83d2b4eb9f95fa888eb91955748
swyx <shawnthe1@gmail.com>
2021-02-18 09:26:25 +0800

    tweak index page (#50)
    Co-authored-by: swyx <swyx@dontemail.me>

--- src/components/Showcase/ShowcaseLineEssay.svelte
+++ src/components/Showcase/ShowcaseLineEssay.svelte
@@ -2,7 +2,7 @@
   export let item
   // $: longDesc = item.description || item.desc || 'No description provided. Suggest one!'
   function formatDate(date) {
-    if (date.getFullYear() > 2019) return date.toDateString().slice(4,10)
+    if (date.getFullYear() > 2020) return date.toDateString().slice(4,10)
     return date ? date.toISOString().slice(0, 10) : '???'
   }
 </script>
@@ -13,26 +13,26 @@
   }
 </style>
 <li class="mb-4 md:mb-8 px-1">
-  <div class="flex">
+  <div class="flex flex-col">
     <span
-      class="inline-flex justify-end items-center text-gray-600 text-xs font-mono whitespace-nowrap">{formatDate(item.effectiveDate) }</span>
+      class="inline-flex uppercase items-center text-gray-400 text-xs font-mono whitespace-nowrap">{formatDate(item.effectiveDate) }</span>
     <a
       href={`/${item.slug}`}
-      class="relative -mr-px inline-flex text-sm text-left leading-5
+      class="relative -mr-px inline-flex text-left leading-5
         mytext font-medium">
       <h3
-        class="mylinkcolors text-base md:text-2xl leading-5
-          font-medium whitespace-normal ml-4 inline"
+        class="mylinkcolors text-base md:text-lg leading-5
+          font-medium whitespace-normal inline"
         class:longTitle={item.title.length > 58}
       >
         {item.title}
       </h3>
-      {#if item.categories}
-        <span class="font-mono text-xs ml-4 hidden md:inline-flex items-center">
-          {item.categories && item.categories.length ? item.categories.map(x => `#${x}`).join(', ') : 'Uncategorized'}
-        </span>
-      {/if}
     </a>
+    {#if item.categories}
+    <div class="mytext font-mono text-xs italic hidden md:inline-flex items-center">
+        {item.categories && item.categories.length ? item.categories.map(x => `#${x}`).join(', ') : 'Uncategorized'}
+    </div>
+    {/if}
   </div>
   <!-- <div class="ml-4 mt-4 p-4">
     <p
