098acb59d8018f7a3c365e68c5661545d489f016
Jamie Tanna <github@jamietanna.co.uk>
2021-05-16 17:07:41 +0100

    Make RSS feeds discoverable (#56)
    To allow folks to subscribe to this site using their feed readers, we
    can add the `rel=alternate` link to the page metadata for the home,
    ideas and subscribe pages.

--- src/routes/ideas/Ideas.svelte
+++ src/routes/ideas/Ideas.svelte
@@ -13,6 +13,7 @@
 <svelte:head>
   <title>swyx | Ideas</title>
   <link rel="canonical" href="https://www.swyx.io/ideas/" />
+  <link rel="alternate" type="application/rss+xml" href="https://swyx.io/rss.xml" />
   <meta property="og:url" content="https://www.swyx.io/ideas/" />
   <meta property="og:type" content="article" />
   <meta property="og:title" content="swyx | Ideas" />
