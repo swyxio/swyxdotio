616c3eea4b96d4daacfe8ee27a7481d1f3a6e0a3
swyx <shawnthe1@gmail.com>
2021-08-08 13:58:20 -0700

    tweak subscibe messaging

--- src/routes/subscribe/Subscribe.svelte
+++ src/routes/subscribe/Subscribe.svelte
@@ -30,17 +30,17 @@
     class="px-4 py-10 max-w-xl mx-auto sm:px-6 sm:py-12 lg:max-w-2xl lg:py-16
       lg:px-8 xl:max-w-3xl"
   >
-    <h2><a href="https://swyx.transistor.fm/"><span class="uppercase text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">The Swyx Newsletter</span></a></h2>
+    <h2><a href="https://swyx.transistor.fm/">✉️ <span class="uppercase text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">The Swyx Newsletter</span></a></h2>
     <div class="mb-8 mytext sm:mx-auto sm:text-center lg:text-left lg:mx-0">
       <p class="text-base font-light text-opacity-75">
-        Join <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">3,000+</span> developers getting updates ✉️
+        Join <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">3,000+</span> developers getting good reads, fun picks, and first looks at my work before I publish!
       </p>
       <SignUp />
       <p class="mt-3 text-sm leading-5 text-opacity-75 italic">
         Too soon!
         <a
-          class="font-medium text-opacity-75
-            underline"
+          class="text-opacity-75
+            hover:underline"
           rel="noopener"
           href="https://twyx.ck.page/posts"
           >Show me what I'm signing up for!</a
@@ -48,9 +48,13 @@
       </p>
     </div>
 
-    <h2><a href="https://swyx.transistor.fm/"><span class="uppercase font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">The Swyx Mixtape</span></a></h2>
-    <div class="text-white">
-      My personal pod where I share 5-10 minute clips from over 250 podcasts! Search "<span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">swyx</span>" wherever fine podcasts are sold.
+    <h2><a href="https://swyx.transistor.fm/">🎧 <span class="uppercase font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">The Swyx Mixtape</span></a></h2>
+    <div class="mytext">
+      My personal pod where I share 5-10 minute clips from over 250 podcasts! <br /> Search "<span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">swyx</span>" wherever fine podcasts are sold
+        (<a class="underline mylinkcolors" href="https://podcasts.apple.com/us/podcast/learn-in-podcast/id1549059398">Apple</a>, 
+        <a class="underline mylinkcolors" href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy50cmFuc2lzdG9yLmZtL2xlYXJuLWluLXBvZGNhc3Q=">Google</a>, 
+        <a class="underline mylinkcolors" href="https://overcast.fm/itunes1549059398">Overcast</a>, 
+        <a class="underline mylinkcolors" href="https://feeds.transistor.fm/learn-in-podcast">RSS</a>).
     </div>
   </article>
   
