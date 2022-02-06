c6269db47e2ef26c444cadfec0282bfca793016b
sw-yx <shawnthe1@gmail.com>
2022-02-07 01:04:07 +0800

    talks upgrade

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,18 +12,49 @@ module.exports = {
 					css: {
 						'--tw-prose-bullets': theme('colors.black'),
             blockquote: {
-              // border: "none",
-              // color: "var(--ifm-color)",
-              // backgroundColor: "transparent",
-              fontSize: "inherit",
-              fontStyle: "inherit",
-              fontWeight: "medium",
+              borderLeft: '3px solid red',
+              fontSize: 'inherit',
+              fontStyle: 'inherit',
+              fontWeight: 'medium',
             },
-            "blockquote p:first-of-type::before": {
-              content: "",
+            'blockquote p:first-of-type::before': {
+              content: '',
             },
-            "blockquote p:last-of-type::after": {
-              content: "",
+            'blockquote p:last-of-type::after': {
+              content: '',
+            },
+
+            'code::before': false,
+            'code::after': false,
+            code: {
+              'border-radius': '0.25rem',
+              padding: '0.15rem 0.3rem',
+              borderWidth: '2px',
+              borderColor: 'rgba(0,0,0,0.1)',
+            },
+            'a:hover': {
+              color: '#31cdce !important',
+              textDecoration: 'underline !important',
+            },
+            a: {
+              color: '#3182ce',
+              textDecoration: 'none',
+            },
+            "a code": {
+              color: 'unset'
+            },
+            "li, ul, ol": {
+              margin: 0
+            },
+            "li > img": {
+              margin: 0,
+              display: 'inline'
+            },
+            'ol > li::marker': {
+              color: 'var(--tw-prose-body)',
+            },
+            'ul > li::marker': {
+              color: 'var(--tw-prose-body)',
             },
 					}
 				}
