d0255536d2b6ed0e8ebab34b2be33f6700d715bf
swyx <shawnthe1@gmail.com>
2023-01-31 03:12:09 -0500

    fix most popular in mobile

--- src/routes/ideas/MostPopular.svelte
+++ src/routes/ideas/MostPopular.svelte
@@ -5,85 +5,85 @@
 		Most Popular
 	</h3>
 
-	<article class="grid grid-cols-[1fr_200px] items-baseline gap-2">
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+	<article class="grid sm:grid-cols-[1fr_200px] items-baseline gap-2">
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>The fastest way to learn</span
 		>
 		<a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/learn-in-public"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Learn in Public</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Learn in Public</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>Luck Surface Area, The 4 Kinds of Luck & beyond</span
 		>
 		<a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/moving-to-a-github-cms"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">How to Create Luck</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">How to Create Luck</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>Split it into Community, Content & Product</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/measuring-devrel"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Measuring DevRel</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Measuring DevRel</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>The future of JS tools & infra from 2020-2030</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/js-third-age"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">The Third Age of JS</span
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">The Third Age of JS</span
 			></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400">On AWS vs Cloudflare</span><a
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400">On AWS vs Cloudflare</span><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/cloudflare-go"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Eating the Cloud</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Eating the Cloud</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>The final frontier of language and infra</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/cloudflare-go"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Self-Prov. Runtimes</span
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Self-Prov. Runtimes</span
 			></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400">The iPhone of System Design</span><a
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400">The iPhone of System Design</span><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/why-temporal"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Why Temporal</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Why Temporal</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>Why it's good, but also has a dark side</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/api-economy"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">The API Economy</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">The API Economy</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>Have a job, but don't BE your job</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/part-time-creator-manifesto"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Part Time Creators</span></a
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Part Time Creators</span></a
 		>
 
-		<span class="text-right  text-gray-600 dark:text-gray-400"
+		<span class="text-right hidden sm:block text-gray-600 dark:text-gray-400"
 			>Don't play games you don't want to win</span
 		><a
 			class="w-full text-gray-900 hover:text-yellow-600 hover:no-underline dark:text-gray-100 dark:hover:text-yellow-100"
 			href="/meta-creator-ceiling"
-			><span class="mb-2 w-full flex-auto text-lg font-bold md:text-xl">Meta-Creator Ceiling</span
+			><span class="mb-2 w-full flex-auto text-lg sm:font-bold md:text-xl">Meta-Creator Ceiling</span
 			></a
 		>
 	</article>
