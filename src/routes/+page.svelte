008885e38df10c65f340d6afe3fe5913165f5ede
swyx.io <shawnthe1@gmail.com>
2025-09-08 15:02:49 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,8 +58,8 @@
 				Writer, Founder, Devtools Startup Advisor
 			</h2>
 			<p class="mb-4 text-gray-700 dark:text-gray-200">
-				I document <a href="https://www.latent.space/p/ai-engineer">the Rise of the AI Engineer</a> on <a href="https://latent.space/">Latent.Space</a>, and am working on
-				<a href="https://smol.ai">the Smol AI company</a> based in San Francisco. I also help devtools startups cross the chasm (devrel, advising and <a href="/portfolio">investing</a>) and will always be the champion for <a href="/learn-in-public"
+				I foster <a href="https://www.latent.space/p/ai-engineer">the Rise of the AI Engineer</a> and am working on
+				<a href="https://latent.space">Latent Space</a>, <a href="https://ai.engineer">AI Engineer</a>, and <a href="https://www.swyx.io/cognition">Cognition</a> based in San Francisco. I also help devtools startups cross the chasm (devrel, advising and <a href="/portfolio">investing</a>) and will always be the champion for <a href="/learn-in-public"
 				>Learning in Public</a
 			>!
 			</p>
