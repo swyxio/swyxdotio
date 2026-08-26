3fce658a3207fdb4b6e42c44ec93716da25c21b5
swyxio <shawnthe1@gmail.com>
2026-08-25 21:56:35 -0700

    feat(draw): add private creative assets and versioned house kits

---
+++ src/routes/tools/api/draw/creative/[...path]/+server.js
@@ -0,0 +1,6 @@
+import { forwardCreativeRequest } from '../../../../../../lib/server/draw-creative-store.js';
+
+export const GET = forwardCreativeRequest;
+export const POST = forwardCreativeRequest;
+export const PUT = forwardCreativeRequest;
+export const DELETE = forwardCreativeRequest;
